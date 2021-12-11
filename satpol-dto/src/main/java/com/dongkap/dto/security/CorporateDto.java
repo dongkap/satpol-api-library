e7f272900bacebcf1efc9b355a67611abc4568a2
Ridla Fadilah <ridla.fadilah@gmail.com>
2021-12-12 01:13:53 +0700

    api corporate

---
+++ satpol-dto/src/main/java/com/dongkap/dto/security/CorporateDto.java
@@ -0,0 +1,31 @@
+package com.dongkap.dto.security;
+
+import com.dongkap.dto.common.BaseAuditDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class CorporateDto extends BaseAuditDto {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8874124628846019913L;
+	private String id;
+	private String corporateCode;
+	private String corporateName;
+	private Boolean corporateNonExpired;
+	private String email;
+	private String address;
+	private String telpNumber;
+	private String faxNumber;
+
+}
