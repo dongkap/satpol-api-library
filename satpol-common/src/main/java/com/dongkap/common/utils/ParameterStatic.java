4972c8bf7e411162bd509046e8ce9ce7214f4769
Ridla Fadilah <ridla.fadilah@gmail.com>
2021-12-30 02:02:46 +0700

    asset dto

--- satpol-common/src/main/java/com/dongkap/common/utils/ParameterStatic.java
+++ satpol-common/src/main/java/com/dongkap/common/utils/ParameterStatic.java
@@ -4,6 +4,7 @@ public final class ParameterStatic {
 
 	public static final String PLATFORM_WEB = "web";
 	public static final String MONTH_PARAMETER = "MONTH.";
+	public static final String PERSIST_DATA = "PERSIST";
 	public static final String INSERT_DATA = "INSERT";
 	public static final String UPDATE_DATA = "UPDATE";
 	public static final String DELETE_DATA = "DELETE";
