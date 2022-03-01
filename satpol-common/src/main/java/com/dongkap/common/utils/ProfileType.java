f695f753a3817c30321dc4b236d66b0a281284eb
Ridla Fadilah <ridla.fadilah@gmail.com>
2022-03-02 06:24:34 +0700

    profile type

---
+++ satpol-common/src/main/java/com/dongkap/common/utils/ProfileType.java
@@ -0,0 +1,11 @@
+package com.dongkap.common.utils;
+
+public final class ProfileType {
+
+    public static final String LOCAL = "local";
+    public static final String HEROKU = "heroku";
+    public static final String DEVELOPMENT = "development";
+    public static final String TESTING = "testing";
+    public static final String PRODUCTION = "production";
+
+}
